01c07243a71250863b76ae8d4df5d125d26e6ea8
huanmingchang <huanming1988@gmail.com>
2021-07-18 16:40:39 +0800

    feat: add render card number and color function

---
+++ main.js
@@ -0,0 +1,39 @@
+const Symbols = [
+  'https://image.flaticon.com/icons/svg/105/105223.svg', // 黑桃
+  'https://image.flaticon.com/icons/svg/105/105220.svg', // 愛心
+  'https://image.flaticon.com/icons/svg/105/105212.svg', // 方塊
+  'https://image.flaticon.com/icons/svg/105/105219.svg', // 梅花
+]
+
+const view = {
+  getCardElement(index) {
+    const number = this.transformNumber((index % 13) + 1)
+    const symbol = Symbols[Math.floor(index / 13)]
+    return `
+     <div class="card">
+      <p>${number}</p>
+      <img src="${symbol}" alt="">
+      <p>${number}</p>
+    </div>
+    `
+  },
+  transformNumber(number) {
+    switch (number) {
+      case 1:
+        return 'A'
+      case 11:
+        return 'J'
+      case 12:
+        return 'Q'
+      case 13:
+        return 'K'
+      default:
+        return number
+    }
+  },
+  displayCards() {
+    const rootElement = document.querySelector('#cards')
+    rootElement.innerHTML = this.getCardElement(51)
+  },
+}
+view.displayCards()
