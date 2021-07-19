a4b58c80e5c74862ea37f14e46117cac1e00d476
huanmingchang <huanming1988@gmail.com>
2021-07-19 09:18:21 +0800

    feat: add random shuffle function

--- main.js
+++ main.js
@@ -33,9 +33,25 @@ const view = {
   },
   displayCards() {
     const rootElement = document.querySelector('#cards')
-    rootElement.innerHTML = Array.from(Array(52).keys())
+    rootElement.innerHTML = utility
+      .getRandomNumberArray(52)
       .map((index) => this.getCardElement(index))
       .join('')
   },
 }
+
+const utility = {
+  getRandomNumberArray(count) {
+    const number = Array.from(Array(count).keys())
+    for (let index = number.length - 1; index > 0; index--) {
+      let randomIndex = Math.floor(Math.random() * (index + 1))
+      ;[number[index], number[randomIndex]] = [
+        number[randomIndex],
+        number[index],
+      ]
+    }
+    return number
+  },
+}
+
 view.displayCards()
